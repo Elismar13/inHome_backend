5275ce5ecf997c22c0a59db4d9a98aae39ad847d
Elismar13 <elismarsilva.5@outlook.com>
2021-01-11 09:41:49 -0300

    :sparkles: (.env file and variables)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-require('dotenv').config();
-
+const path = require('path');
+const loadedEnv = require('dotenv').config({
+    path: path.join(__dirname, '../.env')
+});
+if(loadedEnv.error) throw new Error("No .env file found.");
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -11,7 +14,7 @@ const { errors } = require('celebrate');
 const app = express();
 const server = http.Server(app);
 
-mongoose.connect(process.env.MONGO_URL,
+mongoose.connect(process.env.MONGO_DB_URL,
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
