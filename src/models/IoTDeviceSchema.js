ae6344ec089da50f3d3f1e05ac7400b4024a607f
Elismar13 <elismarsilva.5@outlook.com>
2021-01-12 10:50:35 -0300

    :sparkles: (chore) updating device schema

--- src/models/IoTDeviceSchema.js
+++ src/models/IoTDeviceSchema.js
@@ -5,7 +5,10 @@ const IoTDeviceSchema = new mongoose.Schema({
     DeviceUser: String,
     DeviceID: String,
     DeviceNAME: String,
+    description: String,
     ambient: String,
+    status: String,
+    updated_at: Date,
     location: {
         type: PointSchema,
         index: '2dsphere',
